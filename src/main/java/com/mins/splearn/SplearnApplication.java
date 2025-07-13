82e011d9e3f3c6a1a100022c40f1a7a5f159a862
min <www5981@naver.com>
2025-07-13 23:12:27 +0900

    initial commit

---
+++ src/main/java/com/mins/splearn/SplearnApplication.java
@@ -0,0 +1,14 @@
+package com.mins.splearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplearnApplication.class, args);
+	}
+
+}
+
